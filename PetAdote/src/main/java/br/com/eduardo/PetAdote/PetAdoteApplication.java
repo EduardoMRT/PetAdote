aed3b4599ba051f5a136164654aea065aca66005
EduardoMRT <eduksoficial@gmail.com>
2025-08-25 14:29:39 -0300

    Initial project
    Add a initial base project created using Spring Boot

---
+++ PetAdote/src/main/java/br/com/eduardo/PetAdote/PetAdoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.PetAdote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoteApplication.class, args);
+	}
+
+}
---
+++ PetAdote/src/test/java/br/com/eduardo/PetAdote/PetAdoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.PetAdote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
